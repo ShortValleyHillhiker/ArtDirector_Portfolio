9a5ae58550de4c0c2aceda9bf89ec20507d071d3
ShortValleyHillhiker <114674841+ShortValleyHillhiker@users.noreply.github.com>
2023-03-01 16:34:39 +0100

    update

---
+++ beta/onclick.js
@@ -0,0 +1,31 @@
+const modals = document.querySelectorAll("[data-modal]");
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add("open");
+    const exits = modal.querySelectorAll(".modal-exit");
+    exits.forEach(function (exit) {
+      exit.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.classList.remove("open");
+      });
+    });
+  });
+});
+
+function scrollOne() {
+  var element = document.getElementById("case");
+  element.scrollIntoView();
+}
+
+function scrollTwo() {
+  var element = document.getElementById("om-mig");
+  element.scrollIntoView();
+}
+
+function scrollHome() {
+  var element = document.getElementById("intro");
+  element.scrollIntoView();
+}
