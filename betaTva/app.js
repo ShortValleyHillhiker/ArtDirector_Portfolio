8c99aba55c9d88414f4ce87213a2d9b1898f8d2e
ShortValleyHillhiker <114674841+ShortValleyHillhiker@users.noreply.github.com>
2023-04-11 17:06:17 +0200

    betattt

---
+++ betaTva/app.js
@@ -0,0 +1,59 @@
+let kiss = document.querySelector(".intro-top");
+let bajs = document.querySelector(".intro-parent");
+let testIgen = document.querySelector(".testIgen");
+let test = document.querySelector(".test");
+let botStartSpan = document.querySelectorAll(".bot-mid span");
+let botEndSpan = document.querySelectorAll(".bot-head span");
+let batten = document.querySelector(".bot-btn");
+let body = document.querySelector("body");
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    setTimeout(() => {
+      botStartSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.add("active");
+        }, (idx + 1) * 155);
+      });
+    }, 350);
+
+    setTimeout(() => {
+      testIgen.classList.add("active");
+    }, 0);
+    setTimeout(() => {
+      test.classList.add("active");
+    }, 175);
+
+    setTimeout(() => {
+      botEndSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.add("active");
+        }, (idx + 1) * 155);
+      });
+    }, 1400);
+    setTimeout(() => {
+      bajs.style.transform = "translateY(0)";
+      kiss.style.transform = "translateY(calc(-100%)-1rem)";
+    }, 2300);
+    setTimeout(() => {
+      kiss.style.display = "none";
+      batten.classList.add("active");
+      body.classList.remove("no-scroll");
+    }, 2425);
+  });
+});
+
+// animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
