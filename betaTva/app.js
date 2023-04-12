a40766162ed213265c9b41633a79f296ff806dc9
ShortValleyHillhiker <114674841+ShortValleyHillhiker@users.noreply.github.com>
2023-04-12 14:08:38 +0200

    BetaTwo

--- betaTva/app.js
+++ betaTva/app.js
@@ -1,3 +1,5 @@
+const firstVisit = localStorage.getItem("visited");
+
 let kiss = document.querySelector(".intro-top");
 let bajs = document.querySelector(".intro-parent");
 let testIgen = document.querySelector(".testIgen");
@@ -6,51 +8,82 @@ let botStartSpan = document.querySelectorAll(".bot-mid span");
 let botEndSpan = document.querySelectorAll(".bot-head span");
 let batten = document.querySelector(".bot-btn");
 let body = document.querySelector("body");
+let skitande = document.querySelector(".skitande");
 
-window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
+if (firstVisit == null) {
+  //TEST
+  window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-      botStartSpan.forEach((span, idx) => {
-        setTimeout(() => {
-          span.classList.add("active");
-        }, (idx + 1) * 155);
-      });
-    }, 350);
+      setTimeout(() => {
+        botStartSpan.forEach((span, idx) => {
+          setTimeout(() => {
+            span.classList.add("active");
+          }, (idx + 1) * 155);
+        });
+      }, 350);
 
-    setTimeout(() => {
-      testIgen.classList.add("active");
-    }, 0);
-    setTimeout(() => {
-      test.classList.add("active");
-    }, 175);
+      setTimeout(() => {
+        testIgen.classList.add("active");
+      }, 0);
+      setTimeout(() => {
+        test.classList.add("active");
+      }, 175);
 
-    setTimeout(() => {
-      botEndSpan.forEach((span, idx) => {
-        setTimeout(() => {
-          span.classList.add("active");
-        }, (idx + 1) * 155);
-      });
-    }, 1400);
-    setTimeout(() => {
-      bajs.style.transform = "translateY(0)";
-      kiss.style.transform = "translateY(calc(-100%)-1rem)";
-    }, 2300);
-    setTimeout(() => {
-      kiss.style.display = "none";
-      batten.classList.add("active");
-      body.classList.remove("no-scroll");
-    }, 2425);
+      setTimeout(() => {
+        botEndSpan.forEach((span, idx) => {
+          setTimeout(() => {
+            span.classList.add("active");
+          }, (idx + 1) * 155);
+        });
+      }, 1400);
+      setTimeout(() => {
+        bajs.style.transform = "translateY(0)";
+        kiss.style.transform = "translateY(calc(-100%)-1rem)";
+        skitande.style.minHeight = "auto";
+        skitande.style.marginBottom = "2rem";
+      }, 2300);
+      setTimeout(() => {
+        kiss.style.display = "none";
+        batten.classList.add("active");
+        body.classList.remove("no-scroll");
+      }, 2425);
+    });
   });
-});
+  //TEST
+  localStorage.setItem("visited", 1);
+} else {
+  //TEST
+  setTimeout(() => {
+    kiss.style.display = "none";
+    batten.classList.add("active");
+    body.classList.remove("no-scroll");
+    bajs.style.transform = "translateY(0)";
+    kiss.style.transform = "translateY(calc(-100%)-1rem)";
+    skitande.style.minHeight = "auto";
+    skitande.style.marginBottom = "2rem";
 
-// animation
+    test.classList.add("active");
+    testIgen.classList.add("active");
+    botStartSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      });
+    });
+
+    botEndSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      });
+    });
+  }, 0);
+}
+
+//animation
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
     }
   });
 });
