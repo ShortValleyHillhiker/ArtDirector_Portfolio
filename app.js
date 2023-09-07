b5776d0209a271498bab95034390fd24161c45d3
ShortValleyHillhiker <114674841+ShortValleyHillhiker@users.noreply.github.com>
2023-09-07 15:17:50 +0200

    Page update

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
+//
+
+const elementToMove = document.getElementById("elementToMove");
+let isScrolling = false;
+
+function updateElementPosition() {
+  const totalHeight = document.body.clientHeight - window.innerHeight;
+  const scrollPercentage = (window.scrollY / totalHeight) * 100;
+  const newPosition = scrollPercentage * 2 - 188;
+  elementToMove.style.transform = `translateY(${newPosition}lvh)`;
+  isScrolling = false;
+}
+
+window.addEventListener("scroll", () => {
+  if (!isScrolling) {
+    isScrolling = true;
+    requestAnimationFrame(updateElementPosition);
+  }
+});
+
+//
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
     }
   });
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
-
-function scrollOne() {
-  var element = document.getElementById("one");
-  element.scrollIntoView();
-}
-
-function scrollTwo() {
-  var element = document.getElementById("two");
-  element.scrollIntoView();
-}
-
-function scrollHome() {
-  var element = document.getElementById("home");
-  element.scrollIntoView();
+//
+let kaffeknapp = document.querySelector(".kaffe-popup");
+function kaffe() {
+  kaffeknapp.classList.toggle("active");
 }
