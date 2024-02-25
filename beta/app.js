1d8b8669a4338dbe8a41e5df4d2000cdca97ef31
ShortValleyHillhiker <victor.vennerstrom@gmail.com>
2024-02-25 23:08:26 +0100

    Swup

--- beta/app.js
+++ beta/app.js
@@ -1,4 +1,3 @@
-const swup = new Swup();
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -14,3 +13,61 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+// Siteload
+
+
+const swup = new Swup();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const firstVisit = sessionStorage.getItem("visited");
+
+  let backgroundAni = document.querySelector(".animated-intro");
+  let allLines = document.querySelectorAll(".animated-intro span");
+
+  if (firstVisit == null) {
+    window.addEventListener("DOMContentLoaded", () => {
+      setTimeout(() => {
+        backgroundAni.classList.remove("animate");
+        sessionStorage.setItem("visited", 1);
+      }, 1650);
+
+      setTimeout(() => {
+        backgroundAni.classList.toggle("animated-intro");
+
+      }, 1750);
+    });
+  } else {
+    backgroundAni.classList.remove("animate");
+    backgroundAni.classList.toggle("animated-intro");
+  }
+
+  console.log("Hello world!");
+});
+
+swup.hooks.on('page:view', () => {
+  const firstVisit = sessionStorage.getItem("visited");
+
+  let backgroundAni = document.querySelector(".animated-intro");
+  let allLines = document.querySelectorAll(".animated-intro span");
+
+  if (firstVisit == null) {
+    window.addEventListener("DOMContentLoaded", () => {
+      setTimeout(() => {
+        backgroundAni.classList.remove("animate");
+        sessionStorage.setItem("visited", 1);
+      }, 1650);
+
+      setTimeout(() => {
+        backgroundAni.classList.toggle("animated-intro");
+
+      }, 1750);
+    });
+  } else {
+    backgroundAni.classList.remove("animate");
+    backgroundAni.classList.toggle("animated-intro");
+  }
+
+  console.log("every load");
+});
