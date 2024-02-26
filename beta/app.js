4fd9e097ea0b47c0505416d981ba281525184d53
ShortValleyHillhiker <victor.vennerstrom@gmail.com>
2024-02-26 22:26:02 +0100

    location path name fix

--- beta/app.js
+++ beta/app.js
@@ -17,19 +17,26 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 // Siteload
 function visitCheck() {
-  if (window.location.pathname === '/index.html') {
+  if (window.location.pathname === '/') {
+    console.log("animated-intro start");
+
     const firstVisit = sessionStorage.getItem("visited");
     let backgroundAni = document.querySelector(".animated-intro");
+    let allLines = document.querySelectorAll(".animated-intro span");
+
     if (firstVisit === null) {
       setTimeout(() => {
         backgroundAni.classList.remove("animate");
         sessionStorage.setItem("visited", 1);
       }, 1650);
+
       setTimeout(() => {
         backgroundAni.classList.remove("animated-intro");
-      }, 1750);
+      }, 2350);
+
     } else {
       backgroundAni.classList.remove("animated-intro");
+      console.log("animated-intro removed");
     }
   } else {
     console.log("Not index");
@@ -39,8 +46,10 @@ function visitCheck() {
 
 document.addEventListener('DOMContentLoaded', () => {
   visitCheck();
+  console.log("first load");
 });
 
 swup.hooks.on('page:view', () => {
   visitCheck();
+  console.log("every load");
 });
